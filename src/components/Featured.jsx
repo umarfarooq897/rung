7f1f4740a44bcc162b7e36e32af8ef82096461ec
Shehroz <67423314+Shehroz616@users.noreply.github.com>
2022-06-02 15:01:30 +0500

    remove tabs from featured products

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -5,17 +5,9 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-// import product1_1 from '../assets/images/demos/demo-5/products/product-1-1.jpg';
-// import product1_2 from '../assets/images/demos/demo-5/products/product-1-2.jpg';
-// import product2_1 from "../assets/images/demos/demo-5/products/product-2-1.jpg";
-// import product2_2 from "../assets/images/demos/demo-5/products/product-2-2.jpg";
-// import product3_1 from "../assets/images/demos/demo-5/products/product-3-1.jpg";
-// import product3_2 from "../assets/images/demos/demo-5/products/product-3-2.jpg";
-// import product4_1 from "../assets/images/demos/demo-5/products/product-4-1.jpg";
-// import product4_2 from "../assets/images/demos/demo-5/products/product-4-2.jpg";
 import { NavLink } from "react-router-dom";
 const Featured = (props) => {
-    
+
     const [loading, setLoading] = useState(true);
     const [product, setProduct] = useState([]);
     // const [userdata, setUserdata] = useState()
@@ -64,7 +56,7 @@ const Featured = (props) => {
             // setUser_id(userdata.user.id)
             let product_id = e.target.getAttribute("data-id")
             console.log(user_id)
-            let data = {product_id,user_id}
+            let data = { product_id, user_id }
             // https://cors-anywhere.herokuapp.com/
             var Result = await fetch('https://beta.myrung.com/b/api/v2/wishlists-add-product ', {
                 method: 'POST',
@@ -79,14 +71,13 @@ const Featured = (props) => {
             notifywhish()
         }
         Result = await Result.json()
-        var Data=Result.is_in_wishlist;
+        var Data = Result.is_in_wishlist;
         // console.log(Data);
-        if(Data===true)
-        {
+        if (Data === true) {
             notify_add_whishlist();
         }
-        else if(Data===false){
-            
+        else if (Data === false) {
+
         }
     }
     const options1 = {
@@ -114,127 +105,80 @@ const Featured = (props) => {
             },
         },
     };
-   
+
     return (
         <>
             <div className="container">
                 <div className="heading heading-center mb-3">
                     <h2 className="title">Featured  Products</h2>
-                    {/* <!-- End .title --> */}
-
-                    <ul className="nav nav-pills justify-content-center" role="tablist">
-                        <li className="nav-item">
-                            <a className="nav-link active" id="trendy-all-link" data-toggle="tab" href="#trendy-all-tab" role="tab" aria-controls="trendy-all-tab" aria-selected="true">All</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" id="trendy-women-link" data-toggle="tab" href="#trendy-women-tab" role="tab" aria-controls="trendy-women-tab" aria-selected="false">Women</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" id="trendy-men-link" data-toggle="tab" href="#trendy-men-tab" role="tab" aria-controls="trendy-men-tab" aria-selected="false">Men</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" id="trendy-access-link" data-toggle="tab" href="#trendy-access-tab" role="tab" aria-controls="trendy-access-tab" aria-selected="false">Accessories</a>
-                        </li>
-                    </ul>
                 </div>
                 {/* <!-- End .heading --> */}
 
-                <div className="tab-content tab-content-carousel">
-                    <div className="tab-pane p-0 fade show active" id="trendy-all-tab" role="tabpanel" aria-labelledby="trendy-all-link">
-                        <OwlCarousel className=" owl-theme owl-carousel owl-simple carousel-equal-height carousel-with-shadow" dots={true}
-                            nav={false} margin={20} autoPlay={true} loop={false} {...options1} >
-                            {product ? product.map((item, index) => {
-                                var cat_name = item.category_name
-                                {/* console.log(item) */}
-                                var name = item.name
-                                var calculable_price = item.calculable_price
-                                var currency_symbol = item.currency_symbol
-                                var image = item.thumbnail_image
-                                var product_id = item.id
-                                 var quantity = item.quantity
-                                return (
+                <OwlCarousel className=" owl-theme owl-carousel owl-simple carousel-equal-height carousel-with-shadow" dots={true}
+                    nav={false} margin={20} autoPlay={true} loop={false} {...options1} >
+                    {product ? product.map((item, index) => {
+                        var cat_name = item.category_name
+                        {/* console.log(item) */ }
+                        var name = item.name
+                        var calculable_price = item.calculable_price
+                        var currency_symbol = item.currency_symbol
+                        var image = item.thumbnail_image
+                        var product_id = item.id
+                        var quantity = item.quantity
+                        return (
 
-                                    <div className="product product-2" key={index}>
-                                        <figure className="product-media">
-                                            <NavLink to={`/shop/product/catogeroy/fullwidth/${item.id}`} >
-                                                <img src={'https://beta.myrung.com/b/public/' + item.thumbnail_image} alt="Product image" className="product-image" />
-                                            </NavLink>
-                                            <div className="product-action-vertical">
-                                                <a onClick={addWhishlistHandler} data-id={product_id} className="btn-product-icon btn-wishlist btn-expandable" title="Add to wishlist"><span>add to wishlist</span></a>
-                                            </div>
-                                            {/* <!-- End .product-action --> */}
+                            <div className="product product-2" key={index}>
+                                <figure className="product-media">
+                                    <NavLink to={`/shop/product/catogeroy/fullwidth/${item.id}`} >
+                                        <img src={'https://beta.myrung.com/b/public/' + item.thumbnail_image} alt="Product image" className="product-image" />
+                                    </NavLink>
+                                    <div className="product-action-vertical">
+                                        <a onClick={addWhishlistHandler} data-id={product_id} className="btn-product-icon btn-wishlist btn-expandable" title="Add to wishlist"><span>add to wishlist</span></a>
+                                    </div>
+                                    {/* <!-- End .product-action --> */}
 
-                                            <div onClick={notify} className="product-action product-action-transparent">
-                                                <a onClick={() => {
-                                                    props.addToCartHandler({
-                                                        cat_name: cat_name, name: name,
-                                                        Price:calculable_price, symbol:currency_symbol, product_image: image, product_id: product_id,quantity:1 
-                                                    })
-                                                }} className="btn-product btn-cart"><span>add to cart</span></a>
-                                            </div>
-                                            {/* <!-- End .product-action --> */}
-                                        </figure>
-                                        {/* <!-- End .product-media --> */}
+                                    <div onClick={notify} className="product-action product-action-transparent">
+                                        <a onClick={() => {
+                                            props.addToCartHandler({
+                                                cat_name: cat_name, name: name,
+                                                Price: calculable_price, symbol: currency_symbol, product_image: image, product_id: product_id, quantity: 1
+                                            })
+                                        }} className="btn-product btn-cart"><span>add to cart</span></a>
+                                    </div>
+                                    {/* <!-- End .product-action --> */}
+                                </figure>
+                                {/* <!-- End .product-media --> */}
 
-                                        <div className="product-body">
-                                            <div className="product-cat">
-                                                <a >{cat_name}</a>
-                                            </div>
-                                            {/* <!-- End .product-cat --> */}
-                                            <h3 className="product-title">
-                                                <a >{name}</a></h3>
-                                            {/* <!-- End .product-title --> */}
-                                            <div className="product-price">
-                                                {calculable_price}
-                                            </div>
-                                            {/* <!-- End .product-price --> */}
+                                <div className="product-body">
+                                    <div className="product-cat">
+                                        <a >{cat_name}</a>
+                                    </div>
+                                    {/* <!-- End .product-cat --> */}
+                                    <h3 className="product-title">
+                                        <a >{name}</a></h3>
+                                    {/* <!-- End .product-title --> */}
+                                    <div className="product-price">
+                                        {calculable_price}
+                                    </div>
+                                    {/* <!-- End .product-price --> */}
 
-                                            {/* <!-- <div className="product-nav product-nav-dots">
+                                    {/* <!-- <div className="product-nav product-nav-dots">
                                         <a href="#" className="active" style="background: #2d272b;"><span className="sr-only">Color name</span></a>
                                         <a href="#" style="background: #8f957d;"><span className="sr-only">Color name</span></a>
                                     </div> -->
                                     <!-- End .product-nav --> */}
-                                        </div>
-                                        {/* <!-- End .product-body --> */}
-                                    </div>
-                                );
-                            }) : ""
-                            }
+                                </div>
+                                {/* <!-- End .product-body --> */}
+                            </div>
+                        );
+                    }) : ""
+                    }
 
-                            {/* <!-- End .product --> */}
+                    {/* <!-- End .product --> */}
 
-                            {/* <!-- End .owl-carousel --> */}
-                        </OwlCarousel>
-                    </div>
-
-                    {/* <!-- .End .tab-pane --> */}
-                    {/* <div className="tab-pane p-0 fade" id="trendy-women-tab" role="tabpanel" aria-labelledby="trendy-women-link"> */}
-                    {/* <OwlCarousel className="owl-carousel owl-simple carousel-equal-height carousel-with-shadow" 
-                            margin={20} loop={false} {...options1}  >
-
-                        </OwlCarousel> */}
                     {/* <!-- End .owl-carousel --> */}
-                    {/* </div> */}
-                    {/* <!-- .End .tab-pane --> */}
-                    {/* <div className="tab-pane p-0 fade" id="trendy-men-tab" role="tabpanel" aria-labelledby="trendy-men-link"> */}
-                    {/* <OwlCarousel className="owl-carousel owl-simple carousel-equal-height carousel-with-shadow" 
-                               margin={20} loop={false} {...options1}  >
-
-
-
-                        </OwlCarousel> */}
-                    {/* <!-- End .owl-carousel --> */}
-                    {/* </div> */}
-                    {/* <!-- .End .tab-pane --> */}
-                    {/* <div className="tab-pane p-0 fade" id="trendy-access-tab" role="tabpanel" aria-labelledby="trendy-access-link"> */}
-                    {/* <OwlCarousel className="owl-carousel owl-simple carousel-equal-height carousel-with-shadow" 
-                             margin={20} loop={false} {...options1}   >
-                        </OwlCarousel> */}
-                    {/* <!-- End .owl-carousel --> */}
-                    {/* </div> */}
-                    {/* <!-- .End .tab-pane --> */}
-                </div>
-                {/* <!-- End .tab-content --> */}
+                </OwlCarousel>
+                
             </div>
             <div class="mb-5"></div>
             {/* <!-- End .mb-5 --> */}
